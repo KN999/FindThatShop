8ccff55062840e6fc2f16120425f6f8ef45b7659
Navin <nk02031999@gmail.com>
2019-09-02 23:38:41 +0530

    Trying Heroku

--- server/app.js
+++ server/app.js
@@ -14,6 +14,7 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
+
 router.use(bodyParser.json())
 
 app.use('/user', userRoute);
